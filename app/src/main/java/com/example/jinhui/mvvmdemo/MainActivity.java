d47e840bf3bccf3d0f27b4a144c67d5493d48fa9
jinhuizxc <1004260403@qq.com>
2018-07-23 00:30:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jinhui/mvvmdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jinhui.mvvmdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 尝试MVVM
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
